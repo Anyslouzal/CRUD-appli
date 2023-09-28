adbd375168154920e699edbeaba56ab5f91f20f5
Anyslouzal <anyslouzal7@gmail.com>
2023-09-28 21:55:55 +0100

    update

--- create.php
+++ create.php
@@ -1,34 +1,49 @@
 <?php
-require "./config.php";
+require "./config.php"; // Include your database configuration file
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>create user</title>
+    <title>Create User</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.min.css">
 </head>
 <body>
   <form action="create.php" method="post">
-  <label for="name">name</label>
+    <label for="name">Name</label>
     <input type="text" name="name">
-    <label for="name">old</label>
+    <label for="old">Old</label>
     <input type="text" name="old">
-    <label for="name">email</label>
+    <label for="email">Email</label>
     <input type="text" name="email">
-    <label for="name">code</label>
+    <label for="code">Code</label>
     <input type="text" name="code">
-    <input type="submit" value="add">
+    <input type="submit" value="Add" name="add">
   </form>
+
+  <?php
+  if(isset($_POST["add"])){
+    $name = $_POST["name"];
+    $old = $_POST["old"];
+    $email = $_POST["email"];
+    $code = $_POST["code"];
+
+    $statement = $conn->prepare("INSERT INTO table_user (nom, old, email, code) VALUES (?, ?, ?, ?)");
+    $statement->bindParam(1,$name);    
+    $statement->bindParam(2,$old);
+    $statement->bindParam(3, $email);
+    $statement->bindParam(4,$code);
+
+
+    if ($statement->execute()) {
+      echo "User added successfully.";
+    } else {
+      echo "Error: " . $conn->error;
+    }
+  }
+  ?>
+
 </body>
 </html>
-
-<?php
-$name=$_POST["name"];
-$old=$_POST["old"];
-$email=$_POST["email"];
-$code=$_POST["code"];
-
-$sql="INSERT INTO table_user ()"
-
-?>
---
+++ read.php
@@ -0,0 +1,48 @@
+<?php
+ require "./config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+           <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>old</th>
+            <th>email</th>
+            <th>code</th>
+            <th>
+                action 
+            </th>
+           </tr>
+        </thead>
+        <tbody>
+            <?php
+            $statement = $conn->prepare("SELECT * from table_user");
+            $statement-> execute();
+            $data=$statement->fetchAll();
+
+            foreach($data as $row)
+                echo" <tr>
+                 <td>".$row['id']."</td>
+                 <td>".$row['nom']."</td>
+                 <td>".$row['old']."</td>
+                 <td>".$row['email']."</td>
+                 <td>".$row['code']."</td>
+                 <td><a href='modify.php'>update</a>
+                 <a href='delete.php'>delete</a>
+                 </td>
+                </tr>"
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
